8a798e9de588c8e0428269011261cf43cda84d4e
alessandrofuda <alessandro.fuda@gmail.com>
2021-11-16 09:32:57 +0100

    alter root .env path ..wip..

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // alter .env path
+        app()->useEnvironmentPath(base_path('..'));
+        dump('dsfkjhdfskjhdhsfkdfs');
+        dd(base_path('..'));
     }
 }
