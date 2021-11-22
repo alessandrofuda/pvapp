590512d9d9bda2d6b2a81add12875e1fc9a88536
alessandrofuda <alessandro.fuda@gmail.com>
2021-11-22 12:30:33 +0100

    vue 404 view

--- frontend/app/src/router/index.js
+++ frontend/app/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import PageNotFound from '../views/errors/PageNotFound.vue'
+
 
 Vue.use(VueRouter)
 
@@ -16,7 +19,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About // () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '*',
+    name: '404 Not Found',
+    component: PageNotFound,
   }
 ]
 
---
+++ frontend/app/src/views/errors/PageNotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-not-found">
+    <h1>404 - Page not Found</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    // name: 'PageNotFound',
+  }
+</script>
