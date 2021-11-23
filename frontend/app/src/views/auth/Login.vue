b6c5e4d188997b95a346696e7d8fc45fc7c9088f
alessandrofuda <alessandro.fuda@gmail.com>
2021-11-23 18:54:11 +0100

    vuejs, login page validation (..wip)

--- frontend/app/src/views/auth/Login.vue
+++ frontend/app/src/views/auth/Login.vue
@@ -11,18 +11,19 @@
                 </h3>
               </b-card-header>
               <b-card-text>
-                <b-form @submit="login" novalidate :validated="validations.bForm">
+                <b-form @submit="login" novalidate>
                   <b-form-group id="email" label-for="email">
                     <b-icon-envelope-fill></b-icon-envelope-fill>
                     <b-form-input
                         id="email"
                         v-model="form.email"
+                        :state="validationEmail"
                         type="email"
                         placeholder="Email"
                         aria-de_____scribedby="input-1-live-feedback"
                         req___uired
                     ></b-form-input>
-                    <b-form-invalid-feedback :state="validations.email" i__d="input-1-li___ve-feedback">This is a required field and must be an email.</b-form-invalid-feedback>
+                    <b-form-invalid-feedback :state="validationEmail" i__d="input-1-li___ve-feedback">This is a required field and must be an email.</b-form-invalid-feedback>
                   </b-form-group>
                   <b-form-group id="password" label-for="password">
                     <b-icon-lock-fill></b-icon-lock-fill>
@@ -53,35 +54,49 @@
 
 <script>
 import AuthLayout from '../layouts/authLayout'
-import { email, required } from '@vuelidate/validators'
+// import { email, required } from '@vuelidate/validators'
 // import useVuelidate from '@vuelidate/core'
-//
+
+
+
+
+
+// TODO disinstallare vuelidate e usare la validazione "core" di bootstrap-vue (con computed()..)
+// https://bootstrap-vue.org/docs/components/form --> Feedback helper
+
+
+
+
+
 export default {
   name: 'Login',
   components: {AuthLayout},
   data() {
     return {
       form: {
-        email: null,
-        name: null,
+        email: '',
+        name: '',
       },
-      year: new Date().getFullYear(),
-      validations: {
-        bForm: null,
-        email: null,
-        password: null
-      }
+      year: new Date().getFullYear()
+    }
+  },
+  computed: {
+    validationEmail() {
+      return this.form.email.length == 0 ? null : true // null,true,false // this.userId.length > 4 && this.userId.length < 13
+    },
+    validationPassword() {
+      return null // null,true,false // this.userId.length > 4 && this.userId.length < 13
     }
   },
   // setup: () => ({ v$: useVuelidate() }),
-  validations () {
-    return {
-      form: {
-        email: { required, email },
-        password: { required }
-      }
-    }
-  },
+  // validations () {
+  //   return {
+  //     form: {
+  //       email: { required, email },
+  //       password: { required }
+  //     }
+  //   }
+  // },
   methods: {
     login(event) {
       event.preventDefault()
