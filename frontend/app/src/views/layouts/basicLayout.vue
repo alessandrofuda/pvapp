5a43e7c5ecb3b28c4a80486f5385b1b9a7e8dfce
alessandrofuda <alessandro.fuda@gmail.com>
2021-11-30 15:54:58 +0100

    logout

--- frontend/app/src/views/layouts/basicLayout.vue
+++ frontend/app/src/views/layouts/basicLayout.vue
@@ -2,16 +2,28 @@
   <div class="basic-layout">
     <b-container>
       <nav id="nav" class="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <a v-if="isLogged" class="b__tn" href="#" @click.prevent="logout">Logout</a>
-        <router-link v-else to="/login">Login</router-link>
+        <span v-if="isLogged" class="mr-2">Hello <b>{{ name }}</b>, </span>
+        <span>
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link> |
+        </span>
+        <span v-if="isLogged">
+          <a href="javascript:void(0)" @click.prevent="logout">Logout</a>
+        </span>
+        <span v-else>
+          <router-link to="/login">Login</router-link> |
+          <router-link to="/register">Register</router-link>
+        </span>
       </nav>
       <main class="basic-layout-main">
+        <b-alert class="my-5 py-3" :show="alert.show" :variant="alert.variant"  dismissible>
+          {{ alert.msg }}
+        </b-alert>
         <router-view></router-view>
+        <b-overlay :show="loading" no-wrap></b-overlay>
       </main>
       <footer class="basic-layout-footer">
-        &copy; Awesome Company
+        {{ app_name }} &copy; {{ year }}
       </footer>
     </b-container>
   </div>
@@ -19,20 +31,29 @@
 <script>
 import Store from '@/store'
 import { mapActions } from "vuex"
-import router from '@/router';
-
-// console.log(Store.state.auth.authenticated)
+// import router from '@/router';
 
 export default {
   name: 'basicLayout',
   data() {
     return {
-      // isLogged: false
+      // name: null,
+      app_name: process.env.VUE_APP_NAME,
+      year: new Date().getFullYear(),
+      alert: {
+        show: false,
+        variant: null,
+        msg: null
+      },
+      loading: false
     }
   },
   computed: {
     isLogged() {
       return Store.state.auth.authenticated || false
+    },
+    name() {
+      return Store.state.auth.user.name || null
     }
   },
   methods: {
@@ -45,22 +66,19 @@ export default {
 
       await window.axios.post('/logout')
       .then(() => {
-        //if(status === 200) {
-          this.signOut()
-          router.push({name:'dashboard'})
-        // }else {
-        //   this.alert.variant = 'danger'
-        //   this.alert.msg = `Invalid status code on login: ${status}`
-        // }
-
+        this.signOut()
+        // router.push({name:'dashboard'}).catch(() => { })
+        this.alert.variant = 'success'
+        this.alert.msg = 'Logged Out Successfully'
       })
-      .catch(() => {
-        // this.alert.variant = 'danger'
-        // this.alert.msg = response ? Object.values(response.data.errors).join(' ') : 'General Error'
+      .catch(({response}) => {
+        console.error(response)
+        this.alert.variant = 'danger'
+        this.alert.msg = response ? Object.values(response.data.errors).join(' ') : 'General Error'
       })
       .finally(() => {
         this.loading = false
-        // this.alert.show = true
+        this.alert.show = true
       })
 
     }
