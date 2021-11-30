a0d06dfa99f7b117f574584a49c09f9c89ec2061
alessandrofuda <alessandro.fuda@gmail.com>
2021-11-30 16:02:37 +0100

    fix

--- frontend/app/src/views/auth/Login.vue
+++ frontend/app/src/views/auth/Login.vue
@@ -1,39 +1,37 @@
 <template>
-  <!--auth-layout class="login-form"-->
-      <div class="login-form">
-        <b-row>
-          <b-col>
-            <b-card>
-              <b-card-header>
-                <h3 class="text-center text-uppercase">
-                  <b-icon-box-arrow-in-right></b-icon-box-arrow-in-right>
-                  <span class="ml-2">Login</span>
-                </h3>
-              </b-card-header>
-              <b-card-text>
-                <login-form />
-              </b-card-text>
-              <b-card-footer class="text-ri">
-                <p class="mt-4 mb-1 text-muted">Non sei iscritto? <a href="/api/register">Iscriviti</a></p>
-                <p class="text-muted"><a href="/api/forgot-password">Password dimenticata?</a></p>
-              </b-card-footer>
-            </b-card>
-            <p class="copy text-center text-muted">PvApp &copy; {{ year }}</p>
-          </b-col>
-        </b-row>
-      </div>
-  <!--/auth-layout-->
+  <div class="login-form">
+    <b-row>
+      <b-col>
+        <b-card>
+          <b-card-header>
+            <h3 class="text-center text-uppercase">
+              <b-icon-box-arrow-in-right></b-icon-box-arrow-in-right>
+              <span class="ml-2">Login</span>
+            </h3>
+          </b-card-header>
+          <b-card-text>
+            <login-form />
+          </b-card-text>
+          <b-card-footer class="text-ri">
+            <p class="mt-4 mb-1 text-muted">Non sei iscritto? <router-link to="/register">Iscriviti</router-link></p>
+            <p class="text-muted"><router-link to="/forgot-password">Password dimenticata?</router-link></p>
+          </b-card-footer>
+        </b-card>
+        <p class="copy text-center text-muted">{{ app_name }} &copy; {{ year }}</p>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
-// import AuthLayout from '../layouts/authLayout'
 import LoginForm from '@/components/auth/loginForm'
 
 export default {
   name: 'Login',
-  components: {LoginForm}, // AuthLayout,
+  components: {LoginForm},
   data() {
     return {
+      app_name: process.env.VUE_APP_NAME,
       year: new Date().getFullYear()
     }
   }
