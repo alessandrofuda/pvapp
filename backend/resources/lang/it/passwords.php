944d00cb9158f9964b0728e7c979a9c7f244ab98
alessandrofuda <alessandro.fuda@gmail.com>
2021-12-01 13:22:47 +0100

    translations

---
+++ backend/resources/lang/it/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'La tua password è stata reimpostata correttamente!',
+    'sent' => 'Abbiamo inviato una mail con il link per il reset della password!',
+    'throttled' => 'Per favore attendi prima di riprovare.',
+    'token' => 'Il token per il reset della password non è valido.',
+    'user' => "Non è stato trovato alcun utente con questo indirizzo email.",
+
+];
