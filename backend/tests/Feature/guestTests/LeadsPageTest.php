9f23c0c737406f593dabd0ace80c7f507a05ec5d
alessandrofuda <alessandro.fuda@gmail.com>
2021-12-21 18:35:22 +0100

    leadsPage tests ..wip..

---
+++ backend/tests/Feature/guestTests/LeadsPageTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature\guestTests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LeadsPageTest extends TestCase
+{
+    public function test_guest_user_can_get_leads()
+    {
+
+    }
+
+    public function test_guest_user_get_NOT_completed_leads_attributes()
+    {
+
+    }
+
+    public function test_guest_user_view_purchase_button()
+    {
+
+    }
+    public function test_guest_user_can_view_Bonifico_payment_form()
+    {
+
+    }
+    public function test_guest_user_cant_view_purchase_button_if_lead_is_not_available_anymore()
+    {
+
+    }
+    public function test_()
+    {
+
+    }
+//    public function test_()
+//    {
+//
+//    }
+//    public function test_()
+//    {
+//
+//    }
+//    public function test_()
+//    {
+//
+//    }
+//    public function test_()
+//    {
+//
+//    }
+//    public function test_()
+//    {
+//
+//    }
+//    public function test_()
+//    {
+//
+//    }
+//    public function test_()
+//    {
+//
+//    }
+//    public function test_()
+//    {
+//
+//    }
+//    public function test_()
+//    {
+//
+//    }
+
+}
