bf79c5226a65decf01b620bfc5d320d0c429960b
alessandrofuda <alessandro.fuda@gmail.com>
2024-05-29 09:50:15 +0200

    upgrade to L10

--- backend/tests/Feature/adminTests/AdminsTest.php
+++ backend/tests/Feature/adminTests/AdminsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\adminTests;
 
 use App\Models\Area;
 use App\Models\Lead;
--- backend/tests/Feature/operatorTests/OperatorsTest.php
+++ backend/tests/Feature/operatorTests/OperatorsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\operatorTests;
 
 use App\Models\Area;
 use App\Models\Lead;
