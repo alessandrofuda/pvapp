cc6aafb12b4aef5d76656c37785b562acd0ac0da
alessandrofuda <alessandro.fuda@gmail.com>
2024-09-17 18:15:46 +0200

    dashboard and $page inertia shared data variable

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 use Inertia\Response;
 
 class DashboardController extends Controller
--- routes/auth.php
+++ routes/auth.php
@@ -9,11 +9,15 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\RegisteredOperatorController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
     Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('operator-register', [RegisteredOperatorController::class, 'create'])->name('operator_register');
+    Route::post('operator-register', [RegisteredOperatorController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
