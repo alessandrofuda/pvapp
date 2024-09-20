84fa48313aa647df2b5d46a407a4302e5e5099ae
alessandrofuda <alessandro.fuda@gmail.com>
2024-09-20 16:08:15 +0200

    refactor

--- app/Http/Controllers/Auth/RegisteredOperatorController.php
+++ app/Http/Controllers/Auth/RegisteredOperatorController.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\SaveOperatorRequest;
 use App\Models\User;
 use Exception;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
