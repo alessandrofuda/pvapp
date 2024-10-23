775dbdf3f433f7f70e5cffa85f4c7c2aa21f36b8
alessandrofuda <alessandro.fuda@gmail.com>
2024-10-23 18:18:57 +0200

    operators table, writing with vue.js v.3 composition API version

--- app/Http/Controllers/OperatorsController.php
+++ app/Http/Controllers/OperatorsController.php
@@ -2,24 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Operator;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class OperatorsController extends Controller
 {
+    public Request $request;
+    public ?string $search;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        $this->search = $request->get('search') ?? null;
+    }
     /**
      * @throws Exception
      */
     public function operators() : Response
     {
         try{
-            $operators = User::role('operator')->with('operator')->get();
+            $operators = User::role('operator')
+                ->with('operator')
+                ->when($this->search, function($query){
+                    $query->where(function($query){
+                        $columns = ['name', 'email'];
+                        foreach ($columns as $column) {
+                            $query->orWhere('users.'.$column, 'LIKE', '%'.$this->search.'%');
+                        }
+                        $query->orWhere('operators.phone', 'LIKE', '%'.$this->search.'%');
+                    });
+                })
+                // ->get();
+                ->paginate(10)
+                ->withQueryString();
+
+            dd($operators);
 
         }catch(Exception $e){
             $err = 'Error in '.__METHOD__.': '.$e->getMessage();
--- resources/js/Pages/Operators/Index.vue
+++ resources/js/Pages/Operators/Index.vue
@@ -1,14 +1,36 @@
 <script setup>
 
-import {Head} from "@inertiajs/vue3";
+import {Head, Link, router} from "@inertiajs/vue3";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import SearchFilter from "@/Components/SearchFilter.vue";
-import {reactive} from "vue";
+import {reactive, watch} from "vue";
+import Pagination from "@/Components/Pagination.vue";
+import Icon from "@/Components/Icon.vue";
+import {pickBy, throttle} from "lodash";
+
+const props = defineProps({
+    operators: Array
+})
+
+const filters = {
+    search: '',
+};
 
 const form = reactive({
     search: filters.search,
-    trashed: filters.trashed,
 });
+
+// Watch the form object deeply with throttling
+watch(
+    form,
+    throttle(() => {
+        console.log(pickBy(form))
+        // Use router to send a request
+        router.get('/operators', pickBy(form), { preserveState: true });
+    }, 150),
+    { deep: true }
+);
+
 
 </script>
 
@@ -21,24 +43,56 @@ const form = reactive({
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    ...see ping CRM > Organizations table...
-
-                    <div class="flex items-center justify-between mb-6">
-                        <search-filter v-model="form.search" class="mr-4 w-full max-w-md" @reset="reset">
-                            <label class="block text-gray-700">Trashed:</label>
-                            <select v-model="form.trashed" class="form-select mt-1 w-full">
-                                <option :value="null" />
-                                <option value="with">With Trashed</option>
-                                <option value="only">Only Trashed</option>
-                            </select>
-                        </search-filter>
-                        <!--Link class="btn-indigo" href="/organizations/create">
+                    <div class="flex items-center justify-between mb-6 p-6">
+                        <search-filter v-model="form.search" class="mr-4 w-full max-w-md" @reset="reset"></search-filter>
+                        <Link class="btn-indigo" href="/operators/create">
                             <span>Create</span>
-                            <span class="hidden md:inline">&nbsp;Organization</span>
-                        </Link--><!--todo-->
+                            <span class="hidden md:inline">&nbsp;Operator</span>
+                        </Link>
                     </div>
+
+                    <div class="bg-white rounded-md shadow overflow-x-auto">
+                        <table class="w-full whitespace-nowrap">
+                            <thead>
+                            <tr class="text-left font-bold">
+                                <th class="pb-4 pt-6 px-6">Name</th>
+                                <th class="pb-4 pt-6 px-6">E-mail</th>
+                                <th class="pb-4 pt-6 px-6" colspan="2">Phone</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="operator in operators.data" :key="operator.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
+                                <td class="border-t">
+                                    <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/organizations/${operator.id}/edit`">
+                                        {{ operator.name }}
+                                        <icon v-if="operator.deleted_at" name="trash" class="shrink-0 ml-2 w-3 h-3 fill-gray-400" />
+                                    </Link>
+                                </td>
+                                <td class="border-t">
+                                    <Link class="flex items-center px-6 py-4" :href="`/operators/${operator.id}/edit`" tabindex="-1">
+                                        {{ operator.email }}
+                                    </Link>
+                                </td>
+                                <td class="border-t">
+                                    <Link class="flex items-center px-6 py-4" :href="`/organizations/${operator.id}/edit`" tabindex="-1">
+                                        {{ operator.operator?.phone }}
+                                    </Link>
+                                </td>
+                                <td class="w-px border-t">
+                                    <Link class="flex items-center px-4" :href="`/organizations/${operator.id}/edit`" tabindex="-1">
+                                        <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400" />
+                                    </Link>
+                                </td>
+                            </tr>
+                            <tr v-if="operators.length === 0">
+                                <td class="px-6 py-4 border-t" colspan="4">No operators found.</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <pagination class="mt-6 p-6" :links="operators.links" />
 
 
                 </div>
